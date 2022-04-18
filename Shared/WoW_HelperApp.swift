fb0f8c9a7be864b89da08900251ee77313907d31
Mikolaj Lukasik <pobe16@gmail.com>
2022-04-18 15:02:10 +0100

    Initial Commit

---
+++ Shared/WoW_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoW_HelperApp.swift
+//  Shared
+//
+//  Created by Mikolaj Lukasik on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WoW_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
